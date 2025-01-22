071f8bb316acb99a11a0014aefc513abdce08a3c
a2jensen <a2jensen@ucsd.edu>
2025-01-21 18:46:47 -0800

    Added README and changed cube shape

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth , window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const geometry = new THREE.BoxGeometry( 10, 10, 10 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const materialLine = new THREE.LineBasicMaterial( { color : 0x000ff });
 const cube = new THREE.Mesh( geometry, material );
@@ -25,8 +25,8 @@ const line = new THREE.Line( geometryLine , material )
 scene.add ( line);
 
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.020;
+    cube.rotation.y += 0.020;
 	renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
