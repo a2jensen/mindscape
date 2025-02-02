a7baf15c00ab00c107bad4c2760e1b56dcdb3ca5
Angelo Jensen <a2jensen@ucsd.edu>
2025-02-01 23:09:16 -0800

    Mindscape text edits

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ npx vite
 
 Useful:
 https://www.reddit.com/r/threejs/comments/1i51qpn/how_to_make_this_asciiart_like_look_in_threejs/
+
+https://www.youtube.com/watch?v=55iwMYv8tGI
--- main.js
+++ main.js
@@ -1,32 +1,42 @@
 import * as THREE from 'three';
 
-
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 15, window.innerWidth / window.innerHeight, 0.1 , 1000)
+const camera = new THREE.PerspectiveCamera( 15, window.innerWidth / window.innerHeight , 0.1 , 1000)
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth , window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const materialLine = new THREE.LineBasicMaterial( { color : 0x000ff });
+/**
+ * Creating the cube as well as text
+ */
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width, canvas.height = 256, 256;
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = "black";
+ctx.font = "30px Garamond";
+ctx.fillText("mmmmmindscapeeeeeeeee", 0, 50);
+ctx.fillText("mmmmmindscapeeeeeeeee", 0, 100);
+ctx.fillText("mmmmmindscapeeeeeeeee", 0, 150);
+ctx.fillText("mmmmmindscapeeeeeeeee", 0, 200);
+
+
+// create a texture from the canvas
+const texture = new THREE.CanvasTexture(canvas);
+
+const geometry = new THREE.BoxGeometry( 10, 10, 10 );
+const material = new THREE.MeshBasicMaterial( { map: texture } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 50;
-
-const points = []
-points.push( new THREE.Vector3( -10, 0 , 0));
-points.push( new THREE.Vector3( 0, 10 ,0));
-points.push( new THREE.Vector3( 10, 0 , 0));
-const geometryLine = new THREE.BufferGeometry().setFromPoints( points );
-const line = new THREE.Line( geometryLine , material )
-scene.add ( line);
+camera.position.z = 60;
 
 function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.001;
+    cube.rotation.y += 0.001;
 	renderer.render( scene, camera );
 }
+
 renderer.setAnimationLoop( animate );
